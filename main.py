0f6fbf4beef0a0bd8f61237b910fd40f778b6b75
RusPP <63382777+RusPP@users.noreply.github.com>
2022-11-20 01:50:40 +0300

    regularization

--- main.py
+++ main.py
@@ -8,31 +8,36 @@ def main():
     original_image = original_image.convert('L')
     original = np.array(original_image)
 
-    noise = noise_matrix(original.shape, 0.01)
-    blur = blur_matrix(10, 3, original, 0)
+    noise = noise_matrix(original.shape, 1)
+    blur = blur_matrix(10, 3, original)
 
+    # Blur + noisy
     distorted = distort_image(original, blur, noise)
     distorted_image = Image.fromarray(distorted)
 
+    # Inverse filtration
     restored_inv = inverse_filtration(distorted, blur, noise)
     restored_image_inv = Image.fromarray(restored_inv)
 
+    # Wiener
     wiener_const = 0.000001
     restored_wiener = wiener_filtration(distorted, blur, wiener_const)
     restored_image_wiener = Image.fromarray(restored_wiener)
 
-    blur_p = blur
-    blur_m = blur_matrix(10, 3, original, 1)
-    restored_lucy = richardson_lucy(distorted, blur_p, blur_m, 30)
+    # Lucy richardson
+    restored_lucy = richardson_lucy(distorted, blur, 1)
     restored_image_lucy = Image.fromarray(restored_lucy)
-    restored_image_lucy.show()
 
-    interactive(distorted_image, restored_image_inv, restored_image_wiener)
+    # Tikhonov regularization
+    restored_reg = regularization(distorted, blur, noise)
+    restored__image_reg = Image.fromarray(restored_reg)
+
+    interactive(distorted_image, restored_image_inv, restored_image_wiener, restored_image_lucy, restored__image_reg)
 
 
-def interactive(distorted_image, restored_image_inv, restored_image_wiener):
+def interactive(distorted_image, restored_image_inv, restored_image_wiener, restored_image_lucy, restored_image_reg):
     description = ["Choose image: ", "1. Distorted image ", "2. Inverse filtration ", "3. Wiener filtration ",
-                   "0. Close"]
+                   "4. Richardson-Lucy ", "5. Tikhonov regularization ", "0. Close"]
     for i in description:
         print(i)
     work = True
@@ -44,6 +49,10 @@ def interactive(distorted_image, restored_image_inv, restored_image_wiener):
             restored_image_inv.show()
         elif tools == '3':
             restored_image_wiener.show()
+        elif tools == '4':
+            restored_image_lucy.show()
+        elif tools == '5':
+            restored_image_reg.show()
         elif tools == '0':
             work = False
         else:
@@ -72,17 +81,13 @@ def add_noise(image, factor):
     return noisy_img
 
 
-def blur_matrix(size, sigma, image, sign):
+def blur_matrix(size, sigma, image):
     # Centers of filter
     x0 = size // 2
     y0 = size // 2
 
-    if sign == 0:
-        x = np.arange(0, size, dtype=float)
-        y = np.arange(0, size, dtype=float)[:, np.newaxis]
-    elif sign == 1:
-        x = -np.arange(0, size, dtype=float)
-        y = -np.arange(0, size, dtype=float)[:, np.newaxis]
+    x = np.arange(0, size, dtype=float)
+    y = np.arange(0, size, dtype=float)[:, np.newaxis]
 
     exp_part = ((x - x0) ** 2 + (y - y0) ** 2) / (2 * sigma ** 2)
     gaussian = 1 / (2 * np.pi * sigma ** 2) * np.exp(-exp_part)
@@ -117,21 +122,48 @@ def wiener_filtration(distorted, blur, wiener_const):
     return restored
 
 
-def richardson_lucy(distorted, blur_p, blur_m, iteration):
+def richardson_lucy(distorted, blur, iteration):
     distorted_spec = np.fft.fft2(distorted)
-    blur_p_spec = np.fft.fft2(blur_p)
-    blur_m_spec = np.fft.fft2(blur_m)
+    blur_spec = np.fft.fft2(blur)
 
     new_restored = distorted
     new_restored_spec = distorted_spec
     for i in range(iteration):
         restored = new_restored
         restored_spec = new_restored_spec
-        _hf = np.fft.ifft2(blur_p_spec * restored_spec)
+        _hf = np.fft.ifft2(blur_spec * restored_spec)
         _ghf = distorted / _hf
-        _hghf = np.fft.ifft2(blur_m_spec * np.fft.fft2(_ghf))
-        new_restored = restored * _hghf
-    return np.abs(new_restored)
+        _hghf = np.fft.ifft2(blur_spec * np.fft.fft2(_ghf))
+        new_restored = np.abs(restored * _hghf)
+    return new_restored
+
+
+def regularization(distorted, blur, noise):
+    distorted_spec = np.fft.fft2(distorted)
+    blur_spec = np.fft.fft2(blur)
+
+    laplas_matrix = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+    laplas = np.zeros(distorted.shape)
+    laplas[:laplas_matrix.shape[0], :laplas_matrix.shape[1]] = np.copy(laplas_matrix)
+    laplas_spec = np.fft.fft2(laplas)
+
+    _lambda = 1.0
+    _alpha = 10000000.0
+
+    while True:
+        restored_spec = np.conj(blur_spec) / (
+                np.abs(blur_spec) ** 2 + _lambda * np.abs(laplas_spec) ** 2) * distorted_spec
+        restored = np.fft.ifft2(restored_spec)
+        discrepancy_spec = distorted_spec - blur_spec * restored_spec
+        discrepancy = np.fft.ifft2(discrepancy_spec)
+        discrepancy_norm = np.sum(discrepancy ** 2)
+        noise_norm = np.sum(noise ** 2)
+        if discrepancy_norm < noise_norm - _alpha:
+            _lambda += 10
+        elif discrepancy_norm > noise_norm + _alpha:
+            _lambda -= 10
+        else:
+            return np.abs(restored)
 
 
 if __name__ == '__main__':
